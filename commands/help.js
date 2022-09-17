53701a72efd3ab321f1353bfcac146108bfe694a
skeddles <skeddy13@yahoo.com>
2022-09-17 15:29:41 -0400

    added actual help command text

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,54 @@ module.exports.config = {
 	type: 1
 };
 
+const INTROTEXT = `\`\`\`
+Hello there! Welcome to the world of Lozpekamon!
+Lozpekistan is inhabited by creatures called Lozpekamon!
+Help us learn more about these creatures by capturing
+them in lozpekaballs, so they can be studied!
+
+To catch a lozpekamon, you first must find one!
+They've recently been seen wandering around
+the #lozpekistan-fields channel, so keep an eye out!
+
+When you find one, act quickly! They may run
+away, or another researcher might get it first!
+
+To use a lozpekaball, react to the monster
+image that pops up with the lozpekaball emoji!
+
+They aren't always so easy to catch though,
+and if you miss they'll run away!
+
+There are a few commands you can use, starting with:
+    !lozpekamon               [alias: !lzpkmn or !lm]
+
+To view the monsters you've already recorded:
+    !lozpekamon dex           [alias: d]
+
+To learn more about a monster you caught:
+    !lozpekamon dex 0   (replace 0 with monster #)
+      (if enough are caught, a description is unlocked)
+
+To see the top researchers:
+    !lozpekamon researchers   [alias: r]
+
+Give a lozpekamon to another player:
+    !lozpekmon give @user_tag 0  (replace 0 with monster #)
+      (feel free to trade for monsters, art, or whatever!)
+
+In order to learn more about the Lozpekamon, we need to
+microchip them and release them into the wild. If enough people
+do this, we'll unlock more information on that monster!
+    !lozpekamon release 0      (replace 0 with monster #)
+
+Good luck on your Lozpekamon journey!
+
+>>Also try Lozpekistan Monster Farm!
+https://skeddles.itch.io/lozpekistan-monster-farm
+
+\`\`\``;
+
 module.exports.execute = async function (interaction) {
-	await interaction.reply({content: 'help yourself', ephemeral: true });
+	await interaction.reply({content: INTROTEXT, ephemeral: true });
 }	
