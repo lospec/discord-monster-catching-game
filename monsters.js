762f52485a089ea3c6b17b61c913e77979d40a5a
skeddles <skeddy13@yahoo.com>
2022-12-07 21:30:20 -0500

    moved Monster class to monster.js and added basic tests

--- monsters.js
+++ monsters.js
@@ -1,6 +1,8 @@
 import { Store } from 'data-store';
 import getRarity from './utilities/calculate-rarity.js';
 import runAway from './utilities/runaway.js';
+import { MonsterGameConfig } from './bot.js';
+import randomElement from "./utilities/random-element.js";
 export const MonsterStore = new Store({ path: './_data/monster-data.json' });
 	console.log(MonsterStore)
 	const monsters = MonsterStore.get();
@@ -14,7 +16,58 @@ export function setRunaway(length) {
 export const MONSTERS = {};
 export const monstersAutocomplete = [];
 
-console.log(MonsterStore.get())
+export class Monster {
+    constructor(id, name, type, level, stats) {
+        this.id = id;
+        this.monName = name;
+        this.type = type;
+        this.stats = stats;
+        this.level = level;
+		this.personality = randomElement(MonsterGameConfig.get('personalities'));
+    }
+
+    static fromString(str) {
+		let [id, name, type, level, stats] = str.split(',');
+        return new Monster(id, name, type, level, stats);
+    }
+
+    static rollNew(monsterId, name) {
+        let monster = MonsterStore.get(String(monsterId));
+		console.log('rolling new monster:', monsterId, name, monster)
+		let newStats = {};
+		Object.entries(monster.stats).forEach(s => newStats[s[0]] = s[1].min);
+
+		let statPointsPool = 8;
+		Object.values(monster.resistances).forEach(r => statPointsPool-=r);
+		Object.values(monster.weaknesses).forEach(w => statPointsPool+=w);
+		Object.values(newStats).forEach(s => statPointsPool-=s.min);
+
+		while (statPointsPool > 0) {
+			let stat = randomElement(monster.stats);
+			if (newStats[stat] + 1 > monster.stats[stat].max) continue;
+			newStats[stat]++;
+		}
+
+        return new Monster(monsterId, name, MonsterStore.get(monsterId+".type"), 1, newStats);
+    }
+
+    toString() {
+        return `${this.id},${this.monName},${this.type},${this.personality},${this.level},${this.statsText}`;
+    }
+
+    output() {
+        return `${this.monName} is a ${this.type} type monster with ${this.statsText}.`;
+    }
+
+	get statsText () {
+		console.log('get statstest:', {
+			'stats': this.stats,
+			'list': Object.entries(this.stats),
+
+		})
+		return Object.entries(this.stats).map(s => s[0].toUpperCase()+': '+s[1]).join(' | ');
+	}
+}
 
 Object.keys(monsters).forEach(monsterId => {
 	console.log(monsterId)
--- tests/monsters.js
+++ tests/monsters.js
@@ -1,10 +1,34 @@
+import { TextInputStyle } from 'discordjs14';
 import {MonsterGameClient} from '../bot.js';
-import {MONSTERS,Pool,monstersAutocomplete} from '../monsters.js';
+import {MONSTERS,Pool,monstersAutocomplete,Monster} from '../monsters.js';
 
 describe('Monsters', function() {
 
+
+	describe('Monster class', function() {
+		it('exists', function() {
+			expect(Monster).toBeDefined();
+		});	
+
+		describe('instantiated with new', function() {
+			let monster = new Monster('1','test','test',1,{});
+			it('is defined', function() {expect(monster).toBeDefined();});
+		});
+
+		describe('instantiated with fromString', function() {
+			let monster = Monster.fromString('1,test,test,1,{}');
+			it('is defined', function() {expect(monster).toBeDefined();});
+		});
+
+		describe('instantiated with rollNew', function() {
+			let monster = Monster.rollNew(1,'namey');
+			it('is defined', function() {expect(monster).toBeDefined();});
+		});
+	});
+
 	it('exports MONSTERS object', function() {
 		expect(MONSTERS).toBeDefined();
+		expect(Object.keys(MONSTERS).length).toBeGreaterThan(0);
 	});
 
 	it('exports Pool array', function() {
